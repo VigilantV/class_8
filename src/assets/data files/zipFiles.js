d9325fc7501b20ca218efd0ce75aee19574bd122
Mehrab Gheibi <mehrabgheibi@gmail.com>
2023-11-16 23:59:16 +0330

    add in_class_4

--- src/assets/data files/zipFiles.js
+++ src/assets/data files/zipFiles.js
@@ -1,11 +1,13 @@
 import in_class_1 from "../zip files/in_class_1.zip";
 import in_class_2 from "../zip files/in_class_2.zip";
 import in_class_3 from "../zip files/in_class_3.zip";
+import in_class_4 from "../zip files/in_class_4.zip";
 
 const zipFiles = [
   { title: "فایل اول", fileName: "in_class_1", file: in_class_1 },
   { title: "فایل دوم", fileName: "in_class_2", file: in_class_2 },
   { title: "فایل سوم", fileName: "in_class_3", file: in_class_3 },
+  { title: "فایل چهارم", fileName: "in_class_4", file: in_class_4 },
 ];
 
 export default zipFiles;
